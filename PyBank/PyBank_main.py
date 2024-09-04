c1f1e52cd956fb07bf2488c94538303bed978d76
MakRamsey <164778729+MakRamsey@users.noreply.github.com>
2024-09-03 22:41:48 -0400

    Submission File Upload

--- PyBank/PyBank_main.py
+++ PyBank/PyBank_main.py
@@ -1,68 +1,76 @@
-#Please note: This code was worked on with collaboration with academic tutors and classmates
-#First import the necessary modules to execute this code
-#os for file path simplicity & csv for reading CSV files
+# Import the necessary modules: os for file path simplicity & csv for reading CSV files
 
 import os
 import csv
 
-#assign variable to hold path/address to CSV file
+# assign variable to hold path/address to CSV file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-#open and read the CSV file by initiating a with loop that while open, variable csvreader is going to hold the readable version of the csv file
+# open and read the CSV file by initiating a with loop that while open, variable csvreader is going to hold the readable version of the csv file
 with open(csvpath) as csvfile:
-    #utilizing the reader function imported via the csv module, we are reading the 'csvfile' (variable holding the opened path) and storing the value in csvreader
+
+    # utilizing the reader function imported via the csv module, we are reading the 'csvfile' (variable holding the opened path) and storing the value in csvreader
     csvreader = csv.reader(csvfile, delimiter=',')
-    #Skipping the header row of the data to exclude column titles (stored for potential call back later)
+
+    # Skipping the header row of the data to exclude column titles (stored for potential call back later)
     header = next(csvreader, None)
 
-    #Set variables in order to track desired results
+    # Set variables in order to track desired results
     month_list = []
     net_start = 0
     last_rev = None
     change_list = []
 
-    #initiate for loop that cycles through each row within the readable csv file providing a mechanism to derive insights from the data
+    # initiate for loop that cycles through each row within the readable csv file providing a mechanism to derive insights from the data
     for rows in csvreader:
-        #print(rows)
-        #adding or "appending" each row to a callback list which can be len"ed" to obtain the total number of months
+
+        # adding or "appending" each row to a callback list which can be len"ed" to obtain the total number of months
         month_list.append(rows)
-        #because net_start variable was previously defined as zero, we can iterate it with the corresponding integer value from each rows to obtain a total count
+
+        # because net_start variable was previously defined as zero, we can iterate it with the corresponding integer value from each rows to obtain a total count
         net_start += int(rows[1])
-        #initiate profit_or_loss variable that will enter into the below if loop (for the second row once last_rev resets with a value
-        #this will serve as a relative starting point for revenue change calculations
+
+        # initiate profit_or_loss variable that will enter into the below if loop (for the second row once last_rev resets with a value
+        # this will serve as a relative starting point for revenue change calculations
         profit_or_loss = int(rows[1])
 
         if last_rev is not None:
-            #on second pass this if loop is entered which calculates the delta between each entries revenue numbers
+
+            # on second pass this if loop is entered which calculates the delta between each entries revenue numbers
             delta = profit_or_loss - last_rev
-            #adding or "appending each delta to the change_list initiated above
+
+            # adding or "appending each delta to the change_list initiated above
             change_list.append(delta)
 
         last_rev = profit_or_loss
 
-    #open a dictionary in order to combine gathered data in order to later oput the greatest increase/decrease numbers along with their respective dates
+    # open a dictionary in order to combine gathered data in order to later oput the greatest increase/decrease numbers along with their respective dates
     Date_PL = {}
-    #resetting the csv file in ordre to allow it to be "re-read"
+
+    # resetting the csv file in ordre to allow it to be "re-read"
     csvfile.seek(0)
-    #must initiate another variable to hold the second "read" of the file once reset from above
+
+    # must initiate another variable to hold the second "read" of the file once reset from above
     csvreader2 = csv.reader(csvfile, delimiter=',')
+
     # Skipping the header row of the data to exclude column titles
     next(csvreader2, None)
 
-    #initiate second for loop that cycles through each row within the readable csv file (version 2) providing a mechanism to derive insights from the data
+    # initiate second for loop that cycles through each row within the readable csv file (version 2) providing a mechanism to derive insights from the data
     for rows in csvreader2:
-        #print(rows)
-        #storing the key-value pairs as "DATE":int(REVENUE) within our dictionary
-        Date_PL[rows[0]] = int(rows[1])
-    #print(Date_PL)
 
-    #define max and min change within out delta or change list representing the net change from day to day
+        # storing the key-value pairs as "DATE":int(REVENUE) within our dictionary
+        Date_PL[rows[0]] = int(rows[1])
+
+    # define max and min change within out delta or change list representing the net change from day to day
     max_change = max(change_list)
     min_change = min(change_list)
-    #define two variables to hold the string values of the months when the greatest increase/deacrease occurred
+
+    # define two variables to hold the string values of the months when the greatest increase/deacrease occurred
     month_increase = ""
     month_decrease = ""
-    #initiate another for loop to cycle through each row, adding the change list value to the corresponding entry and identifying if it was max or min
+
+    # initiate another for loop to cycle through each row, adding the change list value to the corresponding entry and identifying if it was max or min
     for i, rows in enumerate(Date_PL.items()):
         if i != 0:
             Date_PL[rows[0]] = change_list[i - 1]
@@ -71,13 +79,13 @@ with open(csvpath) as csvfile:
             if change_list[i -1] == min_change:
                 month_decrease = rows[0]
 
-    #calculating average change between each row's revenue by using our change_list
+    # calculating average change between each row's revenue by using our change_list
     average_change = sum(change_list) / len(change_list)
 
-    #establishing month count varaible to hold/represent the length of the month list previously derived
+    # establishing month count varaible to hold/represent the length of the month list previously derived
     month_count = len(month_list)
 
-    #printing full analysis according to requested formatting
+    # printing full analysis according to requested formatting
     print("Financial Analysis")
     print("---------------------------")
     print(f'Total Months: {month_count}')
@@ -86,7 +94,7 @@ with open(csvpath) as csvfile:
     print(f'Greatest Increase in Profits: {month_increase} (${max(Date_PL.values())})')
     print(f'Greatest Decrease in Profits: {month_decrease} (${min(Date_PL.values())})')
 
-#create string variable to hold program results for future export
+# create string variable to hold program results for future export
 output = '''
 Financial Analysis
 ---------------------------
@@ -96,9 +104,9 @@ Average Change: $-8311.11
 Greatest Increase in Profits: Aug-16 ($1862002)
 Greatest Decrease in Profits: Feb-14 ($-1825558)'''
 
-#assign variable to hold output path for analysis text file
+# assign variable to hold output path for analysis text file
 output_path = os.path.join('Analysis', "Py_Bank_Analysis.txt")
 
-#export results to separate text file within out "Analysis" folder
+# export results to separate text file within out "Analysis" folder
 with open(output_path, 'w') as output_file:
     print(output, file=output_file)
--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -1,25 +1,25 @@
-#Please note: This code was worked on with collaboration with academic tutors and classmates
-#First import the necessary modules to execute this code
-#os for file path simplicity & csv for reading CSV files
+# First import the necessary modules: os for file path simplicity & csv for reading CSV files
 
 import os
 import csv
 
-#assign variable to hold path/address to CSV file
+# assign variable to hold path/address to CSV file
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-#open and read the CSV file by initiating a with loop that while open, variable csvreader is going to hold the readable version of the csv file
+# open and read the CSV file by initiating a with loop that while open, variable csvreader is going to hold the readable version of the csv file
 with open(csvpath) as opening:
-    #utilizing the reader function imported via the csv module, we are reading the 'csvfile' (variable holding the opened path) and storing the value in csvreader
+
+    # utilizing the reader function imported via the csv module, we are reading the 'csvfile' (variable holding the opened path) and storing the value in csvreader
     csvreader = csv.reader(opening, delimiter=",")
-    #Skipping the header row of the data to exclude column titles (stored for potential callback later)
+
+    # Skipping the header row of the data to exclude column titles (stored for potential callback later)
     header = next(csvreader, None)
     
-    #Set variable "candidate" to hold a dictionary with keys = "candidate name" and value = "candidate's votes
+    # Set variable "candidate" to hold a dictionary with keys = "candidate name" and value = "candidate's votes
     candidate = {}
 
-    #initiate for loop that cycles through each row within the readable csv file providing a mechanism to derive insights from the data
-    #this loop will keep track of each row/vote and attribute that vote to the correct candidate based on their respective names
+    # initiate for loop that cycles through each row within the readable csv file providing a mechanism to derive insights from the data
+    # this loop will keep track of each row/vote and attribute that vote to the correct candidate based on their respective names
     for row in csvreader:
         name = row[2]
         if name in candidate:
@@ -29,28 +29,24 @@ with open(csvpath) as opening:
         else:
             candidate[name] = 1
 
-    #print(candidate) for confirmation of correct values within dictionary
-
-    #determine total votes by adding all the values within our dictionary
+    # determine total votes by adding all the values within our dictionary
     total_votes = candidate["Charles Casper Stockham"] + candidate["Diana DeGette"] + candidate["Raymon Anthony Doane"]
-    #print(total_votes)
 
-    #determine each candidate's respective vote percentage, store in a variable and round according to proper formatting
+    # determine each candidate's respective vote percentage, store in a variable and round according to proper formatting
     candidate_one_pct = round((candidate["Charles Casper Stockham"] / total_votes) * 100, 3)
     candidate_two_pct = round((candidate["Diana DeGette"] / total_votes) * 100, 3)
     candidate_three_pct = round((candidate["Raymon Anthony Doane"] / total_votes) * 100, 3)
 
 
-    #initiate a list to hold the found percent values to zip with dictionary below for final print
+    # initiate a list to hold the found percent values to zip with dictionary below for final print
     pct_list = [candidate_one_pct, candidate_two_pct, candidate_three_pct]
-    #print(pct_list) for confirmation
 
-    #assign variables for unique string values that represent each candidate within the election
+    # assign variables for unique string values that represent each candidate within the election
     candidate_one = "Charles Casper Stockham"
     candidate_two = "Diana DeGette"
     candidate_three = "Raymon Anthony Doane"
 
-    #initiate if/and statements to determine winner
+    # initiate if/and statements to determine winner
     if candidate_one_pct > candidate_two_pct and candidate_one_pct > candidate_three_pct:
         winner = candidate_one
     elif candidate_two_pct > candidate_one_pct and candidate_two_pct > candidate_three_pct:
@@ -58,7 +54,7 @@ with open(csvpath) as opening:
     elif candidate_three_pct > candidate_one_pct and candidate_three > candidate_two_pct:
         winner = candidate_three
 
-    #print results according to proper/desired format
+    # print results according to proper/desired format
     print("Election Results")
     print("                ")
     print('------------------------------')
@@ -76,7 +72,7 @@ with open(csvpath) as opening:
     print("                ")
     print('------------------------------')
 
-#create string variable to hold program results for future export
+# create string variable to hold program results for future export
 output = '''
 Election Results
                 
@@ -98,11 +94,10 @@ Winner: Diana DeGette
                 
 ------------------------------'''
 
-#print(output)
 
-#assign variable to hold output path for analysis text file
+# assign variable to hold output path for analysis text file
 output_path = os.path.join('Analysis', "Py_Poll_Analysis.txt")
 
-#export results to separate text file within out "Analysis" folder
+# export results to separate text file within out "Analysis" folder
 with open(output_path, 'w') as output_file:
     print(output, file=output_file)
